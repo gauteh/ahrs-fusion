1d4fd9d464b6287b6eff9fee78f71d5b2d388a71
Gaute Hope <eg@gaute.vetsj.com>
2021-11-30 22:17:17 +0100

    add methods to access current output from filter + setting quaternion + docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,109 @@
 #![cfg_attr(not(test), no_std)]
 
-// extern crate ahrs;
-
 mod nxp;
 
 #[derive(Debug)]
 pub struct NxpFusion {
     freq: f32,
-    nxp: nxp::Nxp
+    nxp: nxp::Nxp,
 }
 
 impl NxpFusion {
+    /// Initializes the 9DOF Kalman filter.
+    ///
+    /// freq: The sensor sample rate in Herz (`s^-1`).
     pub fn new(freq: f32) -> NxpFusion {
-        NxpFusion { freq, nxp: unsafe { nxp::nxp_c_begin(freq) } }
+        NxpFusion {
+            freq,
+            nxp: unsafe { nxp::nxp_c_begin(freq) },
+        }
     }
 
-    pub fn update(&mut self, gx: f32, gy: f32, gz: f32, ax: f32, ay: f32, az: f32, mx: f32, my: f32, mz: f32) {
+    /// Updates the filter with new gyroscope, accelerometer, and
+    /// magnetometer data. For roll, pitch, and yaw the accelerometer values can be
+    /// either m/s^2 or g, but for linear acceleration they have to be in g.
+    ///
+    /// 9DOF orientation function implemented using a 12 element Kalman filter
+    ///
+    ///
+    /// * gx The gyroscope x axis. In DPS.
+    /// * gy The gyroscope y axis. In DPS.
+    /// * gz The gyroscope z axis. In DPS.
+    /// * ax The accelerometer x axis. In g.
+    /// * ay The accelerometer y axis. In g.
+    /// * az The accelerometer z axis. In g.
+    /// * mx The magnetometer x axis. In uT.
+    /// * my The magnetometer y axis. In uT.
+    /// * mz The magnetometer z axis. In uT.
+    pub fn update(
+        &mut self,
+        gx: f32,
+        gy: f32,
+        gz: f32,
+        ax: f32,
+        ay: f32,
+        az: f32,
+        mx: f32,
+        my: f32,
+        mz: f32,
+    ) {
         let nxp = &mut self.nxp as *mut nxp::Nxp;
         unsafe {
             nxp::nxp_c_update(nxp, gx, gy, gz, ax, ay, az, mx, my, mz);
         }
+    }
+
+    pub fn freq(&self) -> f32 {
+        self.freq
+    }
+
+    pub fn roll(&self) -> f32 {
+        self.nxp.PhiPl
+    }
+
+    pub fn pitch(&self) -> f32 {
+        self.nxp.ThePl
+    }
+
+    pub fn yaw(&self) -> f32 {
+        self.nxp.PsiPl
+    }
+
+    /// Returns the quaternion `[w, x, y, z]`.
+    pub fn quaternion(&self) -> [f32; 4] {
+        [
+            self.nxp.qPl.q0,
+            self.nxp.qPl.q1,
+            self.nxp.qPl.q2,
+            self.nxp.qPl.q3,
+        ]
+    }
+
+    /// Set the quaternion to `[w, x, y, z]`.
+    pub fn set_quaternion(&mut self, q: [f32; 4]) {
+        self.nxp.qPl.q0 = q[0];
+        self.nxp.qPl.q1 = q[1];
+        self.nxp.qPl.q2 = q[2];
+        self.nxp.qPl.q3 = q[3];
+    }
+
+    /// Get the linear acceleration part of the acceleration value given to update (in `g`, `[x, y, z]`).
+    pub fn linear_acceleration(&self) -> [f32; 3] {
+        [self.nxp.aSePl[0], self.nxp.aSePl[1], self.nxp.aSePl[2]]
+    }
+
+    /// Get the gravity vector from the gyroscope values (in `g`, `[x, y, z]`).
+    pub fn gravity_vector(&self) -> [f32; 3] {
+        [
+            self.nxp.gSeGyMi[0],
+            self.nxp.gSeGyMi[1],
+            self.nxp.gSeGyMi[2],
+        ]
+    }
+
+    /// Get the geomagnetic vector in global frame (in `uT`, `[x, y, z]`).
+    pub fn geomagnetic_vector(&self) -> [f32; 3] {
+        [self.nxp.mGl[0], self.nxp.mGl[1], self.nxp.mGl[2]]
     }
 }
 
@@ -40,5 +124,12 @@ mod tests {
 
         nxp.update(1., 2., 3., 4., 5., 6., 0., 0., 0.);
         println!("{nxp:?}");
+    }
+
+    #[test]
+    fn set_and_get_quaternion() {
+        let mut nxp = NxpFusion::new(150.);
+        nxp.set_quaternion([0.5, 1., 2., 3.]);
+        assert_eq!(nxp.quaternion(), [0.5, 1., 2., 3.]);
     }
 }
--- src/nxp.rs
+++ src/nxp.rs
@@ -2,4 +2,5 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(unused)]
+#![allow(deref_nullptr)] // https://github.com/rust-lang/rust-bindgen/issues/1651
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
